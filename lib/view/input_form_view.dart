3341c353580d589db83954d96e7060e0e3b49559
Natdanai Thamthong <Natdanai_Pech@hotmail.com>
2024-01-21 10:29:38 +0700

    Update input_form_view.dart

--- lib/view/input_form_view.dart
+++ lib/view/input_form_view.dart
@@ -8,10 +8,29 @@ class input_form_view extends StatefulWidget {
 }
 
 class _input_form_viewState extends State<input_form_view> {
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Input form"),
+      appBar: AppBar(
+        title:Text("Add Product"),
+      ),
+      body:TextFormField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(45.0)),
+            borderSide: BorderSide(color: Colors.orange),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(45.0)),
+            borderSide: BorderSide(color: Colors.orange),
+          ),
+          hintText: "Product Name",
+          labelText: "Product Name",
+        ),
+      ),
+
     );
   }
 }
